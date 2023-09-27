4a663d200f0df78044c41905812d4e1959019a78
Edwin Festus <edwinfestusdev@gmail.com>
2023-09-27 15:49:05 +0100

    mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
-                  href="#"
+                  href="/"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Home
@@ -32,10 +32,10 @@ const Navbar = () => {
                   About
                 </a>
                 <a
-                  href="#"
+                  href="/billing"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Services
+                  Billing
                 </a>
                 <a
                   href="#"
@@ -44,7 +44,12 @@ const Navbar = () => {
                   Contact
                 </a>
               </div>
+
             </div>
+              <div className='hidden md:block'>
+                <a href="" className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Login</a>
+                <a href="" className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>Sign-up</a>
+              </div>
           </div>
           <div className="-mr-2 flex md:hidden">
             <button
@@ -123,7 +128,13 @@ const Navbar = () => {
           >
             Contact
           </a>
+          <div className='border-b-2 w-sm border-gray-700 pt-[1rem]'></div>
+        <div className='flex flex-col'>
+          <a href="" className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium'>Login</a>
+          <a href="" className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium'>Sign-up</a> 
         </div>
+        </div>
+
       </div>
     </nav>
   );
