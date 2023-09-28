aa6f4d4156980e1de6dd8335c741d2249a93c8c0
Edwin Festus <edwinfestusdev@gmail.com>
2023-09-28 05:10:44 +0100

    finishLanding page v2.1

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='py-32 container px-4 bg-gray-50'>
+        <h1 className='text-3xl font-semibold '>Comming Soon ....</h1>
+    </div>
+  )
+}
+
+export default About
