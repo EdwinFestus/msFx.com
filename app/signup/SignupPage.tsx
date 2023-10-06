4d1204ee41f5fa61f708c2a01f482d92d4652442
Edwin Festus <edwinfestusdev@gmail.com>
2023-10-07 00:43:21 +0100

    user Design2

--- app/signup/SignupPage.tsx
+++ app/signup/SignupPage.tsx
@@ -31,19 +31,19 @@ const SignupPage: React.FC = () => {
 		<form className="bg-white p-10 rounded-lg shadow-lg min-w-full">
 			<h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Create an account</h1>
 			<div>
-				<label className="text-gray-800 font-semibold block my-3 text-md" for="username">Username</label>
+				<label className="text-gray-800 font-semibold block my-3 text-md" id="username">Username</label>
 				<input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="username" />
       </div>
 				<div>
-					<label className="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
+					<label className="text-gray-800 font-semibold block my-3 text-md" id="email">Email</label>
 					<input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
       </div>
 					<div>
-						<label className="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
+						<label className="text-gray-800 font-semibold block my-3 text-md" id="password">Password</label>
 						<input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
       </div>
 						<div>
-							<label className="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password</label>
+							<label className="text-gray-800 font-semibold block my-3 text-md" id="confirm">Confirm password</label>
 							<input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
       </div>
 							<button type="submit"  className="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
